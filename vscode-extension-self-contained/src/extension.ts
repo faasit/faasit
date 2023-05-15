e7ef089d48cfd357f7a6d559a90d2ef2309cc4eb
brody715 <brody71517@gmail.com>
2023-05-15 19:36:57 +0800

    clone from https://github.com/cdietrich/xtext-languageserver-example.git

---
+++ vscode-extension-self-contained/src/extension.ts
@@ -0,0 +1,55 @@
+'use strict';
+
+import * as path from 'path';
+import * as os from 'os';
+
+import {Trace} from 'vscode-jsonrpc';
+import { commands, window, workspace, ExtensionContext, Uri } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
+
+let lc: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+    // The server is a locally installed in src/mydsl
+    let launcher = os.platform() === 'win32' ? 'mydsl-standalone.bat' : 'mydsl-standalone';
+    let script = context.asAbsolutePath(path.join('src', 'mydsl', 'bin', launcher));
+
+    let serverOptions: ServerOptions = {
+        run : { command: script },
+        debug: { command: script, args: [], options: { env: createDebugEnv() } }
+    };
+    
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: ['mydsl'],
+        synchronize: {
+            fileEvents: workspace.createFileSystemWatcher('**/*.*')
+        }
+    };
+    
+    // Create the language client and start the client.
+    lc = new LanguageClient('Xtext Server', serverOptions, clientOptions);
+    
+    var disposable2 =commands.registerCommand("mydsl.a.proxy", async () => {
+        let activeEditor = window.activeTextEditor;
+        if (!activeEditor || !activeEditor.document || activeEditor.document.languageId !== 'mydsl') {
+            return;
+        }
+
+        if (activeEditor.document.uri instanceof Uri) {
+            commands.executeCommand("mydsl.a", activeEditor.document.uri.toString());
+        }
+    })
+    context.subscriptions.push(disposable2);
+    
+    // enable tracing (.Off, .Messages, Verbose)
+    lc.setTrace(Trace.Verbose);
+    lc.start();
+}
+export function deactivate() {
+    return lc.stop();
+}
+function createDebugEnv() {
+    return Object.assign({
+        JAVA_OPTS:"-Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8000,suspend=n,quiet=y"
+    }, process.env)
+}

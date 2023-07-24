4d0c4b124020b62824b21818c6ebbe39643cb073
吴晟昊 <55724610+wushenghao666@users.noreply.github.com>
2023-07-24 20:08:26 +0800

    Feat:[Tencentyun provider] Complete the basic logic of tencentyun provider (#3)
    * feat: add createTrigger of tencentyun provider
    
    * fix: wait function ready when creating trigger
    
    * feat: update the code when the function exists
    
    * feat: delete triggers of function before creating
    
    * feat: use async to be more efficient

--- packages/faasit-plugins/src/providers/tencentyun/index.ts
+++ packages/faasit-plugins/src/providers/tencentyun/index.ts
@@ -1,27 +1,24 @@
 import { faas } from '@faasit/std'
 import { scf } from 'tencentcloud-sdk-nodejs-scf'
-import Admzip from 'adm-zip'
 import path from 'path'
 import dotenv from 'dotenv'
+import {
+  transformCreateFunctionParams,
+  transformCreateTriggerParams,
+  transformInvokeParams,
+  transformUpdateFunctionParams,
+} from './transformer'
 
 dotenv.config({ path: path.resolve(__dirname, './.env') })
 const ScfClient = scf.v20180416.Client
 
-function zipFolderAndEncode(folderPath: string) {
-  const zip = new Admzip()
-  zip.addLocalFolder(folderPath)
-  const zipBuffer = zip.toBuffer()
-  const base64 = zipBuffer.toString('base64')
-  return base64
-}
-
 export default function TencentyunPlugin(): faas.ProviderPlugin {
   const client = new ScfClient({
     credential: {
-      secretId: process.env.SECRET_ID,
-      secretKey: process.env.SECRET_KEY,
+      secretId: process.env.TENCENTYUN_SECRET_ID,
+      secretKey: process.env.TENCENTYUN_SECRET_KEY,
     },
-    region: process.env.REGION,
+    region: process.env.TENCENTYUN_REGION,
   })
 
   return {
@@ -30,52 +27,123 @@ export default function TencentyunPlugin(): faas.ProviderPlugin {
       const { logger } = ctx
       const { app } = input
 
-      logger.info(`tencentyun deploy`)
+      logger.info(`Tencentyun deploy`)
 
-      for (const fn of app.functions) {
-        logger.info(`deploy function ${fn.name}`)
+      // get the set of function name to judge whether the function exists
+      logger.info(`Trying to connect to Tencentyun SCF...`)
+      const functionArray = (await client.ListFunctions({})).Functions
+      const functionNameSet = new Set(
+        functionArray.map((fn) => fn.FunctionName)
+      )
 
-        const params = {
-          FunctionName: fn.name,
-          Code: {
-            ZipFile: zipFolderAndEncode(fn.codeDir),
-          },
-          Runtime: fn.runtime,
-        }
-
-        client.CreateFunction(params).then(
-          (data) => {
-            logger.info(`RequestId: ${data.RequestId}`)
-          },
-          (err) => {
+      app.functions.forEach(async (fn) => {
+        if (!functionNameSet.has(fn.name)) {
+          // create function
+          logger.info(`Create function ${fn.name}`)
+          try {
+            const params = transformCreateFunctionParams(fn)
+            const result = await client.CreateFunction(params)
+            logger.info(`Create function requestId: ${result.RequestId}`)
+          } catch (err) {
             logger.error(`Error happens when creating function ${fn.name}`)
             logger.error(err)
+            return
           }
-        )
-      }
+        } else {
+          // update the code of function
+          logger.info(`Update function ${fn.name}`)
+          try {
+            const params = transformUpdateFunctionParams(fn)
+            const result = await client.UpdateFunctionCode(params)
+            logger.info(`Update function requestId: ${result.RequestId}`)
+          } catch (err) {
+            logger.error(`Error happens when updating function ${fn.name}`)
+            logger.error(err)
+            return
+          }
+        }
+
+        // get the status of the function until ready
+        let status
+        while (
+          status !== 'Active' &&
+          status !== 'CreatFailed' &&
+          status !== 'UpdatFailed'
+        ) {
+          status = (await client.GetFunction({ FunctionName: fn.name })).Status
+          logger.info(`The status of function ${fn.name} is ${status}...`)
+        }
+
+        if (status === 'CreatFailed' || status === 'UpdatFailed') {
+          logger.error(`The status of function ${fn.name} is ${status}`)
+          return
+        }
+
+        // get all triggers
+        const existTriggers = await client.ListTriggers({
+          FunctionName: fn.name,
+        })
+        let triggerAmount = existTriggers.TotalCount
+        if (triggerAmount) {
+          // delete all triggers
+          logger.info(`There are ${triggerAmount} triggers, deleting...`)
+          existTriggers.Triggers?.forEach((trigger) => {
+            logger.info(
+              `Delete trigger ${trigger.TriggerName} of function ${fn.name}`
+            )
+            client.DeleteTrigger({
+              FunctionName: fn.name,
+              Type: trigger.Type,
+              TriggerName: trigger.TriggerName,
+            })
+          })
+
+          // wait the delete process done
+          while (triggerAmount) {
+            logger.info(
+              `Deleteing, there are still ${triggerAmount} triggers...`
+            )
+            triggerAmount = (
+              await client.ListTriggers({
+                FunctionName: fn.name,
+              })
+            ).TotalCount
+          }
+        }
+
+        // create trigger
+        fn.triggers.forEach(async (trigger) => {
+          logger.info(`Create trigger ${trigger.name} of function ${fn.name}`)
+          try {
+            const params = transformCreateTriggerParams(trigger, fn.name)
+            const result = await client.CreateTrigger(params)
+            logger.info(`Create Trigger requestId: ${result.RequestId}`)
+            console.log(result.TriggerInfo)
+          } catch (err) {
+            logger.error(
+              `Error happens when creating trigger ${trigger.name} of function ${fn.name}`
+            )
+            logger.error(err)
+          }
+        })
+      })
     },
 
     async invoke(input, ctx) {
       const { logger } = ctx
 
-      logger.info(`invoke function ${input.funcName}`)
+      logger.info(`Invoke function ${input.funcName}`)
 
-      const params = {
-        FunctionName: input.funcName,
-        ClientContext: JSON.stringify({
-          eventMessage: '123',
-        }),
+      // invoke funtion
+      const params = transformInvokeParams(input.funcName)
+      try {
+        const result = await client.Invoke(params)
+        logger.info(`Invoke requestId: ${result.RequestId}`)
+        console.info(result.Result)
+      } catch (err) {
+        logger.error(`Error happens when invoking function ${input.funcName}`)
+        logger.error(err)
       }
-
-      client.Invoke(params).then(
-        (data) => {
-          logger.info(`RequestId: ${data.RequestId}`)
-          console.info(data.Result)
-        },
-        (err) => {
-          logger.error(err)
-        }
-      )
     },
   }
 }
---
+++ packages/faasit-plugins/src/providers/tencentyun/transformer.ts
@@ -0,0 +1,57 @@
+// For tencentyun provider, transform data from standard IR to params which tencentyun API can use.
+
+import { FunctionTrigger, FunctionType } from '@faasit/std/src/faas'
+import Admzip from 'adm-zip'
+
+function zipFolderAndEncode(folderPath: string) {
+  const zip = new Admzip()
+  zip.addLocalFolder(folderPath)
+  const zipBuffer = zip.toBuffer()
+  const base64 = zipBuffer.toString('base64')
+  return base64
+}
+
+export function transformCreateFunctionParams(fn: FunctionType) {
+  return {
+    FunctionName: fn.name,
+    Code: {
+      ZipFile: zipFolderAndEncode(fn.codeDir),
+    },
+    Runtime: fn.runtime,
+  }
+}
+
+export function transformUpdateFunctionParams(fn: FunctionType) {
+  return {
+    FunctionName: fn.name,
+    Code: {
+      ZipFile: zipFolderAndEncode(fn.codeDir),
+    },
+  }
+}
+
+export function transformInvokeParams(funcName: string) {
+  return {
+    FunctionName: funcName,
+  }
+}
+
+export function transformCreateTriggerParams(
+  trigger: FunctionTrigger,
+  funcName: string
+) {
+  const params = {
+    FunctionName: funcName,
+    TriggerName: trigger.name,
+    Type: '',
+    TriggerDesc: '',
+  }
+  switch (trigger.kind) {
+    case 'http':
+      params.Type = 'apigw'
+      params.TriggerDesc = JSON.stringify({})
+      break
+  }
+
+  return params
+}

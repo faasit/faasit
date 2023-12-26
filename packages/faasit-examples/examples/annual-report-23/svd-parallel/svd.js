033e43b2f5fb39bba9b451f68ad2667cb2560085
brody715 <brody71517@gmail.com>
2023-12-26 04:02:36 +0000

    feat: update parallel svd

--- packages/faasit-examples/examples/annual-report-23/svd-parallel/svd.js
+++ packages/faasit-examples/examples/annual-report-23/svd-parallel/svd.js
@@ -1,35 +1,56 @@
 const numeric = require('numeric');
 
-function concatRows (matrices) {
-  return matrices.reduce((acc, curr) => acc.concat(curr), []);
-}
+// Migrate from https://github.com/Chavelior/Split-and-Merge-SVD/blob/master/split_merge.R
+function parallelSvd (X) {
+  let m = X.length;
+  let Y = [];
+  let U1 = [];
+  let xi = [];
+  let X1 = {};
+  let part_len = Math.floor(Math.sqrt(m));
+  let i = 0;
 
-function parallelSVD ({ X, numSplits }) {
-  const m = X.length;
-  const rowSize = Math.floor(m / numSplits);
-  const subMatrices = [];
+  // the loop is able to be parallelized 
+  while (i < m) {
+    if (i + part_len < m) {
+      xi = X.slice(i, i + part_len);
+    } else {
+      xi = X.slice(i, m);
+    }
 
-  for (let i = 0; i < numSplits; i++) {
-    subMatrices.push(X.slice(i * rowSize, (i + 1) * rowSize));
+    let xi_svd = numeric.svd(xi);
+    let fin;
+
+    if (U1.length === 0) {
+      fin = xi_svd.U;
+    } else {
+      let subyup = numeric.rep([U1.length, xi_svd.U[0].length], 0);
+      let x1 = numeric.blockMatrix([[U1, subyup]]);
+      let subydown = numeric.rep([xi_svd.U.length, U1[0].length], 0);
+      let subydowncom = numeric.blockMatrix([[subydown, xi_svd.U]]);
+      fin = numeric.blockMatrix([[x1], [subydowncom]]);
+    }
+
+    U1 = fin;
+    let d = numeric.diag(xi_svd.S);
+    let yi = numeric.dot(d, numeric.transpose(xi_svd.V));
+    Y = Y.concat(yi);
+    i += part_len;
   }
 
-  const svdResults = subMatrices.map(Xi => numeric.svd(Xi));
+  let y_svd = numeric.svd(Y);
+  X1.u = numeric.dot(U1, y_svd.U);
+  X1.v = y_svd.V;
+  X1.d = numeric.diag(y_svd.S);
 
-  const Y = svdResults
-    .map(({ U, S, V }, i) => numeric.dot(numeric.diag(S), V))
-    .reduce((acc, curr) => numeric.concatRows(acc, curr), []);
-  const svdY = numeric.svd(Y);
-
-  const U_tilde = concatRows(svdResults.map(v => v.U));
-  const finalU = numeric.dot(U_tilde, svdY.V);
-  const finalS = svdY.S;
-  const finalV = svdY.V;
-
-  return { U: finalU, S: finalS, V: finalV };
-
+  return X1;
 }
 
+function singleSvd (X) {
+  return numeric.svd(X)
+}
 
-module.exports = { parallelSVD }
-
-
+const data = [[1, 2], [4, 5], [7, 8], [10, 11]]
+console.log("parallel: ", parallelSvd(data))
+console.log("parallel: ", parallelSvd(data))
+console.log("single:", singleSvd(data))

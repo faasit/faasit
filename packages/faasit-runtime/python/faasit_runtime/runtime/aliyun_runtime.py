42f77cef26d3c2c0ea656e89a9d767b2df68f494
吴晟昊 <wushenghaop@qq.com>
2024-04-14 13:56:54 +0800

    feat: finish aliyun_storage of python

--- packages/faasit-runtime/python/faasit_runtime/runtime/aliyun_runtime.py
+++ packages/faasit-runtime/python/faasit_runtime/runtime/aliyun_runtime.py
@@ -1,13 +1,17 @@
+import time
 from faasit_runtime.runtime import FaasitRuntime
 from alibabacloud_fc_open20210406.client import Client as FC_Open20210406Client
 from alibabacloud_tea_openapi import models as open_api_models
 from alibabacloud_fc_open20210406 import models as fc__open20210406_models
 from alibabacloud_tea_util import models as util_models
 from alibabacloud_tea_util.client import Client as UtilClient
-from typing import Any
+from typing import Any, List
 import os
 import json
 from dotenv import load_dotenv,find_dotenv
+from faasit_runtime.runtime.faasit_runtime import StorageInterface
+import oss2
+
 # 获取用户进程中的环境变量文件
 load_dotenv(find_dotenv(usecwd=True))
 def helper_invoke_aliyun_function(fnName: str, event: Any):
@@ -29,6 +33,7 @@ class AliyunRuntime(FaasitRuntime):
         super().__init__()
         self.event = arg0
         self.context = arg1
+        self._storage = self.AliyunStorage()
 
     def input(self):
         return json.loads(str(self.event))
@@ -43,3 +48,41 @@ class AliyunRuntime(FaasitRuntime):
 
     async def tell(self):
         pass
+
+    class AliyunStorage(StorageInterface):
+        def __init__(self):
+            auth = oss2.Auth(os.environ['ALIBABA_CLOUD_ACCESS_KEY_ID'], 
+                            os.environ['ALIBABA_CLOUD_ACCESS_KEY_SECRET'])
+            endpoint = f'https://{os.environ["ALIBABA_CLOUD_OSS_REGION"]}.aliyuncs.com'
+            self.bucket = oss2.Bucket(auth, 
+                            endpoint, 
+                            os.environ['ALIBABA_CLOUD_OSS_BUCKET_NAME'])
+
+        def put(self, filename, data: bytes) -> None:
+            res = self.bucket.put_object(filename, data)
+            if 200 <= res.resp.status < 300:
+                print(f"[storage put] Put data into {filename} successfully.")
+            else:
+                print(res)
+
+        def get(self, filename, timeout = -1) -> bytes:
+            start_t = time.time()
+            while not self.bucket.object_exists(filename):
+                time.sleep(0.001)
+                if timeout > 0:
+                    if time.time() - start_t > timeout / 1000: return None
+            return self.bucket.get_object(filename).read()
+
+        def list(self) -> List:
+            return [sbj.key for sbj in self.bucket.list_objects_v2().object_list]
+
+        def exists(self, filename: str) -> bool:
+            return self.bucket.object_exists(filename)
+
+        def delete(self, filename: str) -> None:
+            if self.bucket.object_exists(filename):    
+                self.bucket.delete_object(filename)
+                print(f"[storage delete] Delete {filename} successfully.")
+            else:
+                print(f"[storage delete] {filename} is not exist.")
+                

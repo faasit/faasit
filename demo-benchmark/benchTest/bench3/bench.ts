696a27019ea2b849cf1338912df21d1b72cc55d2
hjhk555 <hjhkgame@163.com>
2025-03-13 18:40:56 +0800

    feat: add trigger of poisson distribution

---
+++ demo-benchmark/benchTest/bench3/bench.ts
@@ -0,0 +1,27 @@
+import { Metric, Testcase } from "../../../packages/faasit-benchmark/src";
+
+class Demo implements Testcase{
+    async preTestcase(): Promise<boolean> {
+        return true
+    }
+    async preTest(): Promise<boolean> {
+        return true
+    }
+    async runTest(): Promise<Metric[]> {
+        await new Promise(r => setTimeout(r, 1000))
+        return [{
+            name:"test3_times",
+            value:1,
+            conflict:"append"
+        }]
+    }
+    async postTest(): Promise<void> {
+    }
+
+    async postTestcase(): Promise<void> {
+    }
+}
+
+export function getInstance(): Testcase{
+    return new Demo()
+}

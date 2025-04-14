adf6d1c2cab93c62d70065c60774f86d5c5a26d9
xdydy <dydy@i2ec.top>
2025-04-14 09:22:10 +0800

    benchmark: mlpipe

---
+++ benchmark/mlpipe/data/data_upload.py
@@ -0,0 +1,14 @@
+import oss2
+import os
+import pickle
+auth = oss2.Auth(os.environ['FAASIT_SECRET_ALIYUN_ACCESS_KEY_ID'], os.environ['FAASIT_SECRET_ALIYUN_ACCESS_KEY_SECRET'])
+endpoint = f'https://oss-{os.environ["FAASIT_SECRET_ALIYUN_REGION"]}.aliyuncs.com'
+bucket = oss2.Bucket(auth, endpoint, os.environ['ALIBABA_CLOUD_OSS_BUCKET_NAME'])
+for file in os.listdir('.'):
+    if file.endswith('.npy'):
+        print(f'Uploading {file}...')
+
+        with open(file, 'rb') as f:
+            bucket.put_object(file, pickle.dumps(f.read()))
+        # bucket.put_object(file, file)
+        print(f'Uploaded {file} successfully.')

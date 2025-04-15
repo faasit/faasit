ab2a0c219b61a98c0b24efda773e04213a539960
xdydy <dydy@i2ec.top>
2025-04-15 13:41:30 +0800

    fix: handle decode error

--- tests/sig/redis_test/code/index.py
+++ tests/sig/redis_test/code/index.py
@@ -3,7 +3,7 @@ from faasit_runtime import function, FaasitRuntime
 @function
 def func(frt: FaasitRuntime):
     store = frt.storage
-    result = store.get('hello')
-    return frt.output(result)
+    result = store.get('hello').decode()
+    return frt.output({'hello':result})
 
 func = func.export()
